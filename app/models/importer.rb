601e6132fb6f29dddf9043e7b9f4f460131fe6d2
Sergio Gil <sgilperez@gmail.com>
2010-04-17 22:30:05 +0200

    Some traces and optimizations

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -1,11 +1,12 @@
 class Importer
   def self.import_city(name)
     raise "City not found" unless cities.has_key?(name)
+    puts "Creating city: #{name}"
     city = City.create!(:name => name, :nicename => name.downcase, :ine_id => cities[name]["ine_id"])
     url = "http://www.ine.es/prodyser/callejero/caj110/call_p#{cities[name]['province_id']}_110.zip"
     tmp_file = Rails.root.to_s + "/tmp/#{rand(10**9)}.zip"
-    puts "curl #{url} > #{tmp_file} && unzip -p #{tmp_file} \"*TRAMOS*\" | grep ^#{city.ine_id} | iconv -f ISO-8859-1 -t UTF-8 |cut -c 1-7 | uniq"
     `curl #{url} > #{tmp_file} && unzip -p #{tmp_file} "*TRAMOS*" | grep ^#{city.ine_id} | iconv -f ISO-8859-1 -t UTF-8 |cut -c 1-7 | uniq`.split("\n").each do |district_id|
+      puts "Creating district: #{district_id}, #{name}"
       city.districts.create!(:ine_id => district_id)
     end
   end
@@ -14,7 +15,10 @@ class Importer
     session = IneSession.new
     City.all(:include => :districts).each do |city|
       puts "Getting data for #{city.name}"
+      
       population_page = session.population_page(city)
+      places_type_page = session.places_type_page(city)
+      
       city.districts.each do |district|
         ages = population_page.xpath("//table[@border='1']//tr[position()=1]/td[position()>2]").map {|cell| cell.text.to_i + 2.5}
         cells = population_page.xpath("//table[@border='1']//tr[td[contains(.,'#{district.ine_id}')]]/td[@class='dat']").map {|cell| cell.text.gsub(/[^\d]/, '').to_i}
@@ -26,7 +30,6 @@ class Importer
         end
         age = total / population
         
-        places_type_page = session.places_type_page(city)
         culture_and_sport = places_type_page.at_xpath("//table[@border='1']//tr[td[contains(.,'#{district.ine_id}')]]/td[@class='dat' and position() = 6]").text.gsub(/[^\d]/, '').to_f * 1_000_000 / population
         puts "Culture & Sport in #{district.name}, #{city.name}: #{culture_and_sport}"
         
