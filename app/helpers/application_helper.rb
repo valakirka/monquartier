e53f82cdfb106d50bfbc4ddd624bf4518aa5cb3e
Mari Carmen Gutiérrez <valakirka@gmail.com>
2010-04-18 19:06:05 +0200

    Static map with real coordinates in for district

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def get_gmaps_api_key
+    if RAILS_ENV == "development" || RAILS_ENV == "test"
+      "ABQIAAAAMNg-g2ZZT9PPIzZluAcuNxTJQa0g3IQ9GZqIMmInSLzwtGDKaBRmeiO9H0mVMRaNoIKfOODBKnVLSA"
+    else
+      "ABQIAAAAMNg-g2ZZT9PPIzZluAcuNxTdXksTpGeVaiVmlsQqghUFcmPIVRQDW0ridC190_39H16ENBtNz_sfZw"
+    end
+  end
 end
