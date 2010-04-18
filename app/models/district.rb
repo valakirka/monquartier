2b959cb59a8d9049656bb71e4500f119757c0b98
Sergio Gil <sgilperez@gmail.com>
2010-04-18 14:00:51 +0200

    Deleted use of #one?

--- app/models/district.rb
+++ app/models/district.rb
@@ -6,7 +6,7 @@ class District < ActiveRecord::Base
       city.districts.each do |district|
         points = returning({}) do |h|
           NORMALIZED.each do |attribute|
-            h["#{attribute}_points"] = if city.districts.one?
+            h["#{attribute}_points"] = if city.districts.size <= 1
               250
             else
               ((district[attribute] - ranges[attribute].begin) * (5.0 / (ranges[attribute].end - ranges[attribute].begin)) * 100).round
