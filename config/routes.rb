c7368006a82d7d920584ba1f7859208c02b888da
Sergio Gil <sgilperez@gmail.com>
2010-04-18 21:09:37 +0200

    Links and some more details

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ ActionController::Routing::Routes.draw do |map|
     city.resources :districts
   end
   map.resources :districts
+  map.connect "que-es-monquartier", :controller => "districts", :action => "about"
 end
